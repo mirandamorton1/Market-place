122dc19ead7564a5f3b6924f6a4d6d09ba8011c5
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2023-05-04 13:35:40 -0500

    cards added

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -43,13 +43,19 @@ const Dashboard = () => {
              sx={{  
                 marginBottom: '40px'
             }}/>
-            <Card sx={{ maxWidth: 345 }}>
+            <Typography variant="h4" sx={{ marginBottom: '20px'}}>
+                Popular Prodcucts | New Arrivals
+            </Typography>
+            <Card sx={{ 
+                maxWidth: 345,
+                display: 'inline-block',
+                marginRight: '20px' }}>
                 <CardMedia
                     sx={{ height: 140 }}
                     image="/rice.jpg"
                     title="rice"
             />
-                <CardContent>
+            <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                     Rice | May 4, 2023
                     </Typography>
@@ -58,6 +64,61 @@ const Dashboard = () => {
                     </Typography>
                     <Stack spacing={1}>
                     <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+                    </Stack>
+             </CardContent>
+                <CardActions>
+                <Button size="small" color="inherit" >Contact for Price
+                </Button>
+                <Button size="small" color="inherit">Farm Page</Button>
+                </CardActions>
+             </Card>
+             <Card sx={{ maxWidth: 345, 
+                display: 'inline-block',
+                marginRight: '20px' }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image="/food.jpg"
+                    title="food"
+            />
+            <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Other Food | May 4, 2023
+                    </Typography>
+                    <Typography variant="h6" color="text.secondary">
+                    Category Name
+                    </Typography>
+                    <Stack spacing={1}>
+                    <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+                    </Stack>
+             </CardContent>
+                <CardActions>
+                <Button size="small" color="inherit" >Contact for Price
+                </Button>
+                <Button size="small" color="inherit">Farm Page</Button>
+                </CardActions>
+             </Card>
+             <Divider 
+             sx={{  
+                marginBottom: '40px'
+            }}/>
+            <Typography variant="h4" sx={{ marginBottom: '20px'}}>
+                Featured Products
+            </Typography>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image="/beans.jpg"
+                    title="beans"
+            />
+            <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Beans | May 4, 2023
+                    </Typography>
+                    <Typography variant="h6" color="text.secondary">
+                    Category Name
+                    </Typography>
+                    <Stack spacing={1}>
+                    <Rating name="half-rating-read" defaultValue={4} precision={0.5} readOnly />
                     </Stack>
              </CardContent>
                 <CardActions>
